6b0965078daa5ba8f897852dbbef59956cc4a656
sivagiri0203 <mastersivagiri@gmail.com>
2026-02-03 19:30:04 +0530

    Change in the port number

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.use("/api/amadeus", amadeusRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 async function start() {
   const required = ["MONGO_URI", "JWT_SECRET"];
